95cc864ef81604325e6d8d1bceb835f47ac495a4
Fabian Freund <github@fabi.online>
2024-03-13 01:26:08 +0100

    added documentation

--- lib/src/result.dart
+++ lib/src/result.dart
@@ -4,23 +4,38 @@ import 'package:exceptions/src/data/error_message.dart';
 import 'package:exceptions/src/data/result_exception.dart';
 import 'package:fast_equatable/fast_equatable.dart';
 
+/// A typedef for a function that handles exceptions and their stack trace.
+/// It may return an [ErrorMessage] or null.
 typedef ExceptionHandler = ErrorMessage? Function(
     Exception exception, StackTrace stackTrace);
 
+// A class representing the result of an operation that can either succeed or fail.
 class Result<T> with FastEquatable {
   final dynamic _value;
 
+  /// An error message in case the result represents a failure.
   final ErrorMessage? error;
+
+  /// A getter that returns true if the operation was successful (i.e., no error).
   bool get success => error == null;
 
+  /// A getter that returns the value.
+  /// In cases of an unsuccessuful operation, this might throw a [Exception] depending if the generic type is nullable.
   T get value => _value;
+
+  /// A getter that returns the value if the operation was successful, or null if it wasn't.
   T? get valueOrNull => (success) ? _value : null;
+
+  /// A getter that returns the value if the operation was successful, or throws a [ResultException] if it wasn't.
   T get valueOrException => (success) ? _value : throw ResultException(error!);
 
+  /// Constructor for creating a [Result] instance representing a failure.
   Result.error(ErrorMessage this.error) : _value = null;
 
+  /// Constructor for creating a [Result] instance representing a success.
   Result.value(T this._value) : error = null;
 
+  /// A factory constructor to create a [Result] instance from a function that might throw an exception.
   factory Result.from(T Function() getter,
       {ExceptionHandler? exceptionHandler, String? errorGroup}) {
     assert(
@@ -39,6 +54,7 @@ class Result<T> with FastEquatable {
     }
   }
 
+  /// A static method to create a [Result] instance from an async function that might throw an exception.
   static Future<Result<T>> fromAsync<T>(Future<T> Function() getter,
       {ExceptionHandler? exceptionHandler, String? errorGroup}) async {
     assert(
@@ -64,7 +80,9 @@ class Result<T> with FastEquatable {
   List<Object?> get hashParameters => [_value, error];
 }
 
+/// Extension on [Result] to provide additional functionalities.
 extension ResultExtensions<T> on Result<T> {
+  /// Maps a [Result<T>] to [Result<R>] using the provided function. If the original result is a failure, the same error is retained.
   Result<R> map<R>(R Function(T? value) onSuccess,
       {ExceptionHandler? exceptionHandler, String? errorGroup}) {
     if (!success) {
@@ -75,6 +93,7 @@ extension ResultExtensions<T> on Result<T> {
         exceptionHandler: exceptionHandler, errorGroup: errorGroup);
   }
 
+  /// Similar to [map], but for asynchronous operations. Maps a [Result<T>] to [FutureOr<Result<R>>].
   FutureOr<Result<R>> mapAsync<R>(Future<R> Function(T? value) onSuccess,
       {ExceptionHandler? exceptionHandler, String? errorGroup}) {
     if (!success) {
@@ -85,6 +104,7 @@ extension ResultExtensions<T> on Result<T> {
         exceptionHandler: exceptionHandler, errorGroup: errorGroup);
   }
 
+  /// Allows handling the success and error cases of a [Result] separately.
   R when<R>({
     required R Function(T? value) onSuccess,
     required R Function(ErrorMessage errorMessage) onError,
